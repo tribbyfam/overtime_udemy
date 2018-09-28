ea26ae15c3796bc6b7a9c775481cb3f8a3f9d7a3
Mac <mac@MacBook-Pro.fios-router.home>
2018-09-27 21:57:10 -0500

    created posts with controller and tamplate

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,11 +4,29 @@ describe 'navigate' do
   describe 'index' do
     it 'can be reached successfully' do
       visit posts_path
-      expect(page.status_code).to eql(200)
+      expect(page.status_code).to eq(200)
     end
     it 'has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+  end
+
+  describe 'creation' do
+    it 'has a new form that be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_post_path
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
     end
   end
 end
