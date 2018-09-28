c81d12e57f4a464ddd8768e55890f12286b0574b
Mac <mac@MacBook-Pro.local>
2018-09-28 15:48:43 -0500

    implemented usr connection to posts with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,6 +6,7 @@ describe 'navigate' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
+    
     it 'has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
@@ -14,6 +15,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -24,10 +27,17 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,14 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+
+include Warden::Test::Helpers
+Warden.test_mode!
 
 ActiveRecord::Migration.maintain_test_schema!
 
