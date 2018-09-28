598cca0e4b5d253bc3fef090615b5458b6b059c5
Mac <mac@MacBook-Pro.local>
2018-09-28 13:42:53 -0500

    impemented creation process for post and refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
   end
 
@@ -7,12 +8,25 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:date, :rationale))
-    @post.save
-    redirect_to @post
+    @post = Post.new(post_params)
+
+    if @post.save 
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :new
+    end
   end
 
   def show
+    
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
     @post = Post.find(params[:id])
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -13,14 +13,15 @@ describe 'navigate' do
   end
 
   describe 'creation' do
-    it 'has a new form that be reached' do
+    before do
       visit new_post_path
+    end
+
+    it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form page' do
-      visit new_post_path
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
 
